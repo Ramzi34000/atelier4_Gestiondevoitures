c208c1142f05ff8e20ff9a77b07ba65ef9de9440
ramzi <ramzeddine.saadi@gmail.com>
2026-03-06 22:41:36 -0500

    Ajout methodes classe Employe

--- atelier4_Gestiondevoitures.py
+++ atelier4_Gestiondevoitures.py
@@ -4,3 +4,25 @@ class Employe:
         self.nom = nom
         self.prenom = prenom
         self.voitureService = None
+
+    def afficherInfo(self):
+        print(self.nom, self.prenom, self.numeroPermis)
+
+        if self.voitureService:
+            print("Voiture:", self.voitureService.marque, self.voitureService.matricule)
+        else:
+            print("Pas de voiture de service")
+
+    def affecterVoiture(self, voiture):
+        if self.voitureService :
+            print("Cet employé a déjà une voiture")
+        elif voiture.chauffeur:
+            print("Cette voiture est déjà attribuée")
+        else:
+            self.voitureService = voiture
+            voiture.chauffeur = self
+
+    def retirerVoiture(self):
+        if self.voitureService:
+            self.voitureService.chauffeur = None
+            self.voitureService = None
