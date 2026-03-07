923adcbf37dd2ff4f3e250440dc7cd30891978d0
ramzi <ramzeddine.saadi@gmail.com>
2026-03-06 22:46:15 -0500

    Creation classe Voiture

--- atelier4_Gestiondevoitures.py
+++ atelier4_Gestiondevoitures.py
@@ -26,3 +26,12 @@ class Employe:
         if self.voitureService:
             self.voitureService.chauffeur = None
             self.voitureService = None
+
+
+class Voiture:
+    def __init__(self, matricule, annee, marque, kilometrage):
+        self.matricule = matricule
+        self.annee = annee
+        self.marque = marque
+        self.kilometrage = kilometrage
+        self.chauffeur = None
